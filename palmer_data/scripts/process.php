e830caf52708e09ed97bcfee422204ac6301951f
lfarrell <farrelldlfarrell@gmail.com>
2017-06-11 19:28:27 -0400

    Palmer data finished

--- palmer_data/scripts/process.php
+++ palmer_data/scripts/process.php
@@ -15,23 +15,24 @@ $state_list = array(
     'WA' => 45,
     'WY' => 48
 );
-
+/*
 foreach($state_list as $state => $code) {
     foreach($months as $month) {
         if($month < 10) { $month = "0" . $month; }
 
         $links = [
-            'temp' =>  "http://www.ncdc.noaa.gov/cag/time-series/us/$code/00/tavg/1/$month/1895-2016.csv?base_prd=true&firstbaseyear=1901&lastbaseyear=2000",
-            'precip' => "http://www.ncdc.noaa.gov/cag/time-series/us/$code/00/pcp/1/$month/1895-2016.csv?base_prd=true&firstbaseyear=1901&lastbaseyear=2000",
-            'drought' => "http://www.ncdc.noaa.gov/cag/time-series/us/$code/00/pdsi/1/$month/1895-2016.csv?base_prd=true&firstbaseyear=1901&lastbaseyear=2000",
+            'temp' =>  "https://www.ncdc.noaa.gov/cag/time-series/us/$code/00/tavg/1/$month/1895-2017.csv?base_prd=true&begbaseyear=1901&endbaseyear=2000",
+            'precip' => "https://www.ncdc.noaa.gov/cag/time-series/us/$code/00/pcp/1/$month/1895-2017.csv?base_prd=true&begbaseyear=1901&endbaseyear=2000",
+            'drought' => "https://www.ncdc.noaa.gov/cag/time-series/us/$code/00/pdsi/1/$month/1895-2017.csv?base_prd=true&begbaseyear=1901&endbaseyear=2000",
         ];
 
         foreach($links as $type => $link) {
             $ch = curl_init($link);
-            $fp = fopen("state_data/$type/$state" . '_' . "$month.csv", "wb");
+            $fp = fopen("../data/state_data/$type/$state" . '_' . "$month.csv", "wb");
 
             curl_setopt($ch, CURLOPT_FILE, $fp);
             curl_setopt($ch, CURLOPT_HEADER, 0);
+            curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
 
             curl_exec($ch);
             curl_close($ch);
@@ -41,7 +42,7 @@ foreach($state_list as $state => $code) {
         echo $month . " processed\n";
     }
     echo $state . " processed\n";
-}
+} */
 
 // Aggregate Files
 function build($path, $states, $fh) {
@@ -50,12 +51,11 @@ function build($path, $states, $fh) {
 
         if (($handle = fopen($path . $state_file, "r")) !== FALSE) {
             while (($data = fgetcsv($handle, 1000, ",")) !== FALSE) {
-                if(preg_match('/^\d/', $data[0])) {
-                    $year = substr($data[0], 0, 4);
+                $year = substr($data[0], 0, 4);
+                if(preg_match('/^\d/', $data[0]) && $year >= 2000) {
                     $month = substr($data[0], 4);
                     $data[0] = $year;
                     $data[3] = $month;
-                    $data[4] = $state_name;
 
                     fputcsv($fh, $data);
                 }
@@ -69,69 +69,18 @@ function build($path, $states, $fh) {
 
 $types = ['state_data'];
 $fields = ['drought', 'precip', 'temp'];
-$headers = ['year', 'value', 'anomaly', 'month', 'state'];
+$headers = ['year', 'value', 'anomaly', 'month'];
 
 // Aggregate all by type
-foreach($types as $type) {
-    foreach($fields as $field) {
-        $values = scandir("$type/$field");
+foreach($state_list as $state_name => $state) {
+    foreach($types as $type) {
+        foreach($fields as $field) {
+            $values = scandir("../data/$type/$field");
 
-        $fh = fopen($type . '_' . $field . '_all.csv', 'wb');
-        fputcsv($fh, $headers);
-        build("$type/$field/", $values, $fh);
-        fclose($fh);
-    }
-}
-
-// Aggregate all by state
-$headers[] = 'type';
-$by_states = scandir('all');
-$states = [];
-$us = [];
-
-foreach($by_states as $s) {
-    if(preg_match('/^us/', $s)) {
-        $us[] = $s;
-    } elseif(preg_match('/^\./', $s)) {
-        continue;
-    } else {
-        $states[] = $s;
-    }
-}
-
-$fg = fopen('all_by_state/US.csv', 'wb');
-fputcsv($fg, $headers);
-
-foreach($us as $u) {
-    $type = explode('_', $u)[2];
-
-    if (($handle = fopen("all/$u", "r")) !== FALSE) {
-        while (($data = fgetcsv($handle, 1000, ",")) !== FALSE) {
-            if(preg_match('/^\d/', $data[0])) {
-                $data[5] = $type;
-                fputcsv($fg, $data);
-            }
-        }
-        fclose($handle);
-    }
-}
-fclose($fg);
-
-foreach($state_list as $state => $code) {
-    $ft = fopen("all_by_state/$state.csv", "wb");
-    fputcsv($ft, $headers);
-
-    foreach($by_states as $state_file) {
-        $type = explode('_', $state_file)[2];
-
-        if(($handle = fopen("all/$state_file", "r")) !== FALSE) {
-            while (($data = fgetcsv($handle, 1000, ",")) !== FALSE) {
-                if($data[4] == $state) {
-                    $data[5] = $type;
-                    fputcsv($ft, $data);
-                }
-            }
+            $fh = fopen('../data/state_data/all/' . $state_name . '_' . $field . '_all.csv', 'wb');
+            fputcsv($fh, $headers);
+            build("../data/$type/$field/", $values, $fh);
+            fclose($fh);
         }
     }
-    fclose($ft);
 }
